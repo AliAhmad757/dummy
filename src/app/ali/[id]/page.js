3e68c6c0bc37a0df6b489d2fc601bc6747631e0d
Iqbal <pingiqbal@gmail.com>
2023-08-22 13:42:11 +0500

    update build

--- src/app/ali/[id]/page.js
+++ src/app/ali/[id]/page.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-
 const page = async (props) => {
   let data = await fetch(
     `https://jsonplaceholder.typicode.com/todos/${props.params.id}`
   );
   data = await data.json();
-console.log(data)
+  console.log(data);
   return (
     <>
       <h1>
@@ -18,10 +17,10 @@ console.log(data)
 
 export default page;
 
-export async function generateStaticParams() {
-  let data = await fetch(`https://jsonplaceholder.typicode.com/todos`);
-  data = await data.json();
-  return data.map((user) => ({
-    id: user.id.toString(),
-  }));
-}
+// export async function generateStaticParams() {
+//   let data = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+//   data = await data.json();
+//   return data.map((user) => ({
+//     id: user.id.toString(),
+//   }));
+// }
